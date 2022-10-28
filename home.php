41e2bd70f298bcca21a937aba2db1839237a3114
Your Name <youremail@yourdomain.com>
2022-10-28 19:49:48 +0700

    Add register, login and shopping session function

--- home.php
+++ home.php
@@ -1,29 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
+if (isset($_SESSION["username"])) {
+    $username = $_SESSION["username"];
+    session_write_close();
+} else {
+    session_unset();
+    session_write_close();
+    $url = "./index.php";
+    header("Location: $url");
+}
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pheidip Shop</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css" />
-</head>
-
-<body>
-    <?php
-    include 'navigationBar.php';
-    include 'carousel.php';
-    include 'cardSlider.php';
-
-    ?>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-    <script src="js/main.js"></script>
-</body>
-
-</html>
+?>
+<HTML>
+<HEAD>
+<TITLE>Welcome</TITLE>
+<link href="registerandlogin/assets/css/style.css" type="text/css"
+	rel="stylesheet" />
+<link href="registerandlogin/assets/css/user-registration.css" type="text/css"
+	rel="stylesheet" />
+</HEAD>
+<BODY>
+	<div class="container">
+		<div class="page-header">
+			<span class="login-signup"><a href="logout.php">Logout</a></span>
+		</div>
+		<div class="page-content">Welcome <?php echo $username;?></div>
+	</div>
+</BODY>
+</HTML>

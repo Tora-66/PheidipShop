809992a3c16cee18f939ead92e7fb7a0bbec2a43
TranAn618 <tranlean010198@gmail.com>
2022-11-14 19:44:01 +0700

    Fix

--- detailsBrand.php
+++ detailsBrand.php
@@ -13,33 +13,34 @@ $count = mysqli_num_rows($rs);
 include 'php/htmlHead.php';
 include 'php/sidebar.php';
 ?>
-<caption>
-    <h2><?= $field[1] ?> Detials</h2>
-</caption>
-<table class="table table-hove table-bordered"" width=" 50%">
-
-    <tr>
-        <td rowspan="2"><img src="./<?= $field[2] ?>" alt="Image" width="100px" height="100px"></td>
-        <td>Brand ID: </td>
-        <td><?= $field[0] ?> </td>
-    </tr>
-    <tr>
-        <td>name:</td>
-        <td><?= $field[1] ?></td>
-    </tr>
-    <tr>
-        <td colspan="3">description:</td>
-    </tr>
-    <tr>
-        <td colspan="3"><?= $field[3] ?></td>
-    </tr>
-    <tr>
-        <td align="center" colspan="3">
-            <a href="brand.php">Back to brand list</a> ||
-            <a href="editBrand.php?code=<?= $field[0] ?>">Update</a>
-        </td>
-    </tr>
-</Table>
+<div class="container">
+        <h2><?= $field[1]?> Detials</h2>
+        <br>
+        <a href="brand.php" class="btn btn-outline-info rounded-pill m-0">Back</a>
+        <hr>
+        <table  class="table text-nowrap table-responsive">
+            <tr>
+                <td rowspan="2"><img src="./<?= $field[2]?>" alt="Image" width="250px" height="200px"></td>
+                <td>Brand ID: </td>
+                <td><?= $field[0]?> </td>
+            </tr>
+            <tr>
+                <td>name:</td>
+                <td><?= $field[1]?></td>
+            </tr>
+            <tr>
+                <td colspan="3">description:</td>
+            </tr>
+            <tr>
+                <td colspan="3" ><?=$field[3]?></td>
+            </tr>
+            <tr>
+                <td align="center" colspan="3">
+                        <a href="editBrand.php?code=<?= $field[0]?>" class="btn btn-success rounded-pill m-0">Update</a>
+                </td>
+            </tr>
+        </Table>
+    </div>
 
 <?php
 mysqli_close($conn);
